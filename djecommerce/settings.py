b2efa705da3bc1577a0221b5c63ad62fbf19cc68
Zarak shahjee <47606946+Mohammad-zarak-shahjee@users.noreply.github.com>
2020-10-07 12:21:36 +0530

    secret key

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -7,7 +7,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
+SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 ALLOWED_HOSTS = ['127.0.0.1','localhost','yander.herokuapp.com']
 SECRET_KEY = config('SECRET_KEY')
 
